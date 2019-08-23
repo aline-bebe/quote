e76aa365caae4f939711ee25b364d6626f8365de
aline-bebe <ingabirealine40@gmail.com>
2019-08-23 18:13:40 +0200

    adding alert of delete

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,9 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quote: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Quote(1, 'first quote', 'Find an online version and watch merlin find his son',new Date(2019,3,14)),
+    new Quote(3,'second quote','Diana has her birthday coming up soon',new Date(2019,1,12)),
+    new Quote(2,'third quotes','I have to buy cookies for the parrot',new Date(2019,6,9)),
   ];
 
 words(index){
